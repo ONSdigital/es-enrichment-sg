c4f27c2d6f2dfae4a30f8c432d71494fe651b54b
Mike Candlin <michael.candlin@ons.gov.uk>
2019-07-26 11:24:50 +0100

    fixing branch mixup

--- enrichment_method.py
+++ enrichment_method.py
@@ -3,6 +3,7 @@ import boto3
 import pandas as pd
 import traceback
 import os
+from marshmallow import Schema, fields
 
 
 def _get_traceback(exception):
@@ -11,52 +12,89 @@ def _get_traceback(exception):
     :param exception: Exception object
     :return: string
     """
-    return ''.join(
+    return "".join(
         traceback.format_exception(
             etype=type(exception), value=exception, tb=exception.__traceback__
         )
     )
 
-def get_environment_variable(variable):
-    output = os.environ.get(variable,None)
-    if(output is None):
-        raise ValueError(str(variable)+" config parameter missing")
-    return output
+
+class EnvironSchema(Schema):
+    bucket_name = fields.Str(required=True)
+    responder_lookup_file = fields.Str(required=True)
+    county_lookup_file = fields.Str(required=True)
+    identifier_column = fields.Str(required=True)
+    county_lookup_column_1 = fields.Str(required=True)
+    county_lookup_column_2 = fields.Str(required=True)
+    county_lookup_column_3 = fields.Str(required=True)
+    county_lookup_column_4 = fields.Str(required=True)
+    period_column = fields.Str(required=True)
+    marine_mismatch_check = fields.Str(required=True)
+    missing_county_check = fields.Str(required=True)
+    missing_region_check = fields.Str(required=True)
+
 
 def lambda_handler(event, context):
     """
     Performs enrichment process, joining 2 lookups onto data and detecting anomalies
-    :param event - String: Json string representing input
+    :param event: Json string representing input - String
     :param context:
-    :return final_output - String: Json string representing enriched dataframe
+    :return final_output: Json string representing enriched dataframe - String
     """
     try:
+        schema = EnvironSchema()
+        config, errors = schema.load(os.environ)
+        if errors:
+            raise ValueError(f"Error validating environment params: {errors}")
 
+        bucket_name = config["bucket_name"]
+        responder_lookup_file = config["responder_lookup_file"]
+        county_lookup_file = config["county_lookup_file"]
 
-        bucket_name = get_environment_variable('bucket_name')
-        responder_lookup_file = get_environment_variable('responder_lookup_file')
-        county_lookup_file = get_environment_variable('county_lookup_file')
+        identifier_column = config["identifier_column"]
 
+        county_lookup_column_1 = config["county_lookup_column_1"]
+        county_lookup_column_2 = config["county_lookup_column_2"]
+        county_lookup_column_3 = config["county_lookup_column_3"]
+        county_lookup_column_4 = config["county_lookup_column_4"]
+
+        period_column = config["period_column"]
+
+        marine_mismatch_check = config["marine_mismatch_check"]
+        missing_county_check = config["missing_county_check"]
+        missing_region_check = config["missing_region_check"]
         # Set up clients
-        s3 = boto3.resource('s3', region_name='eu-west-2')
+        s3 = boto3.resource("s3", region_name="eu-west-2")
         # Reads in responder lookup file
         responder_object = s3.Object(bucket_name, responder_lookup_file)
-        responder_content = responder_object.get()['Body'].read()
+        responder_content = responder_object.get()["Body"].read()
 
         # Reads in county lookup file
         county_object = s3.Object(bucket_name, county_lookup_file)
-        county_content = county_object.get()['Body'].read()
+        county_content = county_object.get()["Body"].read()
 
         input_data = pd.read_json(event)
         responder_lookup = pd.read_json(responder_content)
         county_lookup = pd.read_json(county_content)
 
-        enriched_df, anomalies = data_enrichment(input_data,
-                                                 responder_lookup, county_lookup)
+        enriched_df, anomalies = data_enrichment(
+            input_data,
+            responder_lookup,
+            county_lookup,
+            identifier_column,
+            county_lookup_column_1,
+            county_lookup_column_2,
+            county_lookup_column_3,
+            county_lookup_column_4,
+            marine_mismatch_check,
+            missing_county_check,
+            missing_region_check,
+            period_column,
+        )
 
-        json_out = enriched_df.to_json(orient='records')
+        json_out = enriched_df.to_json(orient="records")
 
-        anomaly_out = anomalies.to_json(orient='records')
+        anomaly_out = anomalies.to_json(orient="records")
 
         combined_out = {"data": json_out, "anomalies": anomaly_out}
 
@@ -66,144 +104,145 @@ def lambda_handler(event, context):
 
         return {
             "success": False,
-            "error": "Unexpected method exception {}".format(_get_traceback(exc))
+            "error": "Unexpected method exception {}".format(_get_traceback(exc)),
         }
 
     return final_output
 
 
-def marine_mismatch_detector(data, county_lookup_df, county_lookup_column_3,
-                             county_lookup_column_4, period_column, identifier_column):
+def marine_mismatch_detector(
+    data,
+    county_lookup_df,
+    county_lookup_column_3,
+    county_lookup_column_4,
+    period_column,
+    identifier_column,
+):
     """
-    Detects references that are producing marine
-    but from a county that doesnt produce marine
-    :param data - DataFrame: Input data after having been merged
-            with responder_county_lookup
-    :param county_lookup_df - DataFrame: County_marine_lookup dataframe
-    :param county_lookup_column_3 - String: Column from county lookup
-            to join on (county)
-    :param county_lookup_column_4 - String: Column from county lookup
-            representing whether county produces marine or not
-    :param period_column - String: Column that holds the period
-    :param identifier_column - String: Column that holds
-            the unique id of a row(usually responder id)
-    :return: bad_data_with_marine - DataFrame: Df containing information
-            about any reference that is producing marine when it shouldnt
+    Detects references that are producing marine but from a county that doesnt produce marine
+    :param data: Input data after having been merged with responder_county_lookup - DataFrame
+    :param county_lookup_df: County_marine_lookup dataframe - DataFrame
+    :param county_lookup_column_3: Column from county lookup to join on (county) - String
+    :param county_lookup_column_4: Column from county lookup representing whether county produces marine or not - String
+    :param period_column: Column that holds the period - String
+    :param identifier_column: Column that holds the unique id of a row(usually responder id) - String
+    :return: bad_data_with_marine: Df containing information about any reference that is producing marine when it shouldnt - DataFrame
     """
-    data_with_marine = pd.merge(data,
-                                county_lookup_df,
-                                on=county_lookup_column_3)
-    bad_data_with_marine = data_with_marine[(data_with_marine['land_or_marine'] == 'M') &
-                                            (data_with_marine[county_lookup_column_4] == 'n')]
-    bad_data_with_marine['issue'] = 'Reference should not produce marine data'
-    return bad_data_with_marine[[identifier_column,
-                                 'issue',
-                                 'land_or_marine',
-                                 county_lookup_column_4,
-                                 period_column]]
+    data_with_marine = pd.merge(data, county_lookup_df, on=county_lookup_column_3)
+    bad_data_with_marine = data_with_marine[
+        (data_with_marine["land_or_marine"] == "M")
+        & (data_with_marine[county_lookup_column_4] == "n")
+    ]
+    bad_data_with_marine["issue"] = "Reference should not produce marine data"
+    return bad_data_with_marine[
+        [
+            identifier_column,
+            "issue",
+            "land_or_marine",
+            county_lookup_column_4,
+            period_column,
+        ]
+    ]
 
 
 def missing_county_detector(data, county_lookup_column_3, identifier_column):
     """
-    Detects any references that didnt gain a county
-    on the join with the county lookup
-    :param data - DataFrame: Input data after being combined
-                    with responder_county_lookup
-    :param county_lookup_column_3 - String: Column from
-                    county lookup to join on (county)
-    :param identifier_column - String: Column that holds the unique id
-                    of a row(usually responder id)
-    :return: data_without_county - DataFrame: DF containing information
-                    about any reference without a county.
+    Detects any references that didnt gain a county on the join with the county lookup
+    :param data: Input data after being combined with responder_county_lookup - DataFrame
+    :param county_lookup_column_3: Column from county lookup to join on (county) - String
+    :param identifier_column: Column that holds the unique id of a row(usually responder id) - String
+    :return: data_without_county: DF containing information about any reference without a county. - DataFrame
     """
     data_without_county = data[data[county_lookup_column_3].isnull()]
-    data_without_county['issue'] = 'County missing in lookup'
+    data_without_county["issue"] = "County missing in lookup"
 
-    return data_without_county[[identifier_column, 'issue']]
+    return data_without_county[[identifier_column, "issue"]]
 
 
 def missing_region_detector(data, county_lookup_column_2, identifier_column):
     """
-    Detects any references that do not have a region
-    after merge with county marine lookup
-    :param data - DataFrame: Input data after being combined
-            with responder_county_lookup and county_marine_lookup
-    :param county_lookup_column_2 - String: Column from county lookup
-            that contains region(region)
-    :param identifier_column - String: Column that holds
-            the unique id of a row(usually responder id)
-    :return: data_without_region - DataFrame: DF containing information
-                about any reference without a region.
+    Detects any references that do not have a region after merge with county marine lookup
+    :param data: Input data after being combined with responder_county_lookup and county_marine_lookup - DataFrame
+    :param county_lookup_column_2: Column from county lookup that contains region(region) - String
+    :param identifier_column: Column that holds the unique id of a row(usually responder id) - String
+    :return: data_without_region: DF containing information about any reference without a region. - DataFrame
     """
     data_without_region = data[data[county_lookup_column_2].isnull()]
-    data_without_region['issue'] = 'Region missing in lookup'
+    data_without_region["issue"] = "Region missing in lookup"
 
-    return data_without_region[[identifier_column, 'issue']]
+    return data_without_region[[identifier_column, "issue"]]
 
 
-def data_enrichment(data_df, responder_lookup_df, county_lookup_df):
+def data_enrichment(
+    data_df,
+    responder_lookup_df,
+    county_lookup_df,
+    identifier_column,
+    county_lookup_column_1,
+    county_lookup_column_2,
+    county_lookup_column_3,
+    county_lookup_column_4,
+    marine_mismatch_check,
+    missing_county_check,
+    missing_region_check,
+    period_column,
+):
     """
-    :param data_df: Pandas DataFrame of data to be enriched
-    :param responder_lookup_df: Responder lookup DataFrame
-        (map responder code -> county code)
-    :param county_lookup_df: County lookup DataFrame (map county code -> county name)
-    :return: (Enriched_data - DataFrame:DataFrame of enriched data,
-            Anomalies - DataFrame: DF containing info about data
-            anomalies detected in the process.)
+    :param data_df: DataFrame of data to be enriched - dataframe
+    :param responder_lookup_df: Responder lookup DataFrame (map responder code -> county code) - dataframe
+    :param county_lookup_df: County lookup DataFrame (map county code -> county name) - dataframe
+    :return: (Enriched_data - DataFrame:DataFrame of enriched data, Anomalies - DataFrame: DF containing info about data anomalies detected in the process.)
     """
-
-    identifier_column = get_environment_variable('identifier_column')
-
-    county_lookup_column_1 = get_environment_variable('county_lookup_column_1')
-    county_lookup_column_2 = get_environment_variable('county_lookup_column_2')
-    county_lookup_column_3 = get_environment_variable('county_lookup_column_3')
-    county_lookup_column_4 = get_environment_variable('county_lookup_column_4')
-
-    period_column = get_environment_variable('period_column')
-
-    marine_mismatch_check = get_environment_variable('marine_mismatch_check')
-    missing_county_check = get_environment_variable('missing_county_check')
-    missing_region_check = get_environment_variable('missing_region_check')
 
     # Renaming columns to match what is expected
-    responder_lookup_df.rename(columns={"ref": identifier_column,
-                                        county_lookup_column_3: county_lookup_column_3},
-                               inplace=True)
-    county_lookup_df.rename(columns={"cty_code":
-                                         county_lookup_column_3}, inplace=True)
+    responder_lookup_df.rename(
+        columns={
+            "ref": identifier_column,
+            county_lookup_column_3: county_lookup_column_3,
+        },
+        inplace=True,
+    )
+    county_lookup_df.rename(columns={"cty_code": county_lookup_column_3}, inplace=True)
 
-    enriched_data = pd.merge(data_df, responder_lookup_df,
-                             on=identifier_column, how='left')
+    enriched_data = pd.merge(
+        data_df, responder_lookup_df, on=identifier_column, how="left"
+    )
 
     anomalies = pd.DataFrame()
     # Do Missing County Check here
-    if missing_county_check == 'true':
-        anomalies = missing_county_detector(enriched_data,
-                                            county_lookup_column_3,
-                                            identifier_column)
+    if missing_county_check == "true":
+        anomalies = missing_county_detector(
+            enriched_data, county_lookup_column_3, identifier_column
+        )
 
     # Do Marine mismatch check here
-    if marine_mismatch_check == 'true':
+    if marine_mismatch_check == "true":
 
-        marine_anomalies = marine_mismatch_detector(enriched_data,
-                                                    county_lookup_df,
-                                                    county_lookup_column_3,
-                                                    county_lookup_column_4,
-                                                    period_column,
-                                                    identifier_column)
+        marine_anomalies = marine_mismatch_detector(
+            enriched_data,
+            county_lookup_df,
+            county_lookup_column_3,
+            county_lookup_column_4,
+            period_column,
+            identifier_column,
+        )
 
         anomalies = pd.concat([marine_anomalies, anomalies])
 
-    enriched_data = pd.merge(enriched_data, county_lookup_df[[county_lookup_column_1,
-                                                              county_lookup_column_2,
-                                                              county_lookup_column_3]],
-                             on=county_lookup_column_3, how='left')
+    enriched_data = pd.merge(
+        enriched_data,
+        county_lookup_df[
+            [county_lookup_column_1, county_lookup_column_2, county_lookup_column_3]
+        ],
+        on=county_lookup_column_3,
+        how="left",
+    )
 
-    if missing_region_check == 'true':
+    if missing_region_check == "true":
 
-        region_anomalies = missing_region_detector(enriched_data,
-                                                   county_lookup_column_2,
-                                                   identifier_column)
+        region_anomalies = missing_region_detector(
+            enriched_data, county_lookup_column_2, identifier_column
+        )
 
         anomalies = pd.concat([region_anomalies, anomalies])
 
--- enrichment_wrangler.py
+++ enrichment_wrangler.py
@@ -4,6 +4,7 @@ import boto3
 import pandas as pd
 import os
 import random
+from marshmallow import Schema, fields
 
 
 def _get_traceback(exception):
@@ -13,29 +14,25 @@ def _get_traceback(exception):
     :return: string
     """
 
-    return ''.join(
+    return "".join(
         traceback.format_exception(
             etype=type(exception), value=exception, tb=exception.__traceback__
         )
     )
 
-def get_environment_variable(variable):
-    output = os.environ.get(variable,None)
-    if(output is None):
-        raise ValueError(str(variable)+" config parameter missing")
-    return output
 
 def get_from_s3(bucket_name, key):
     """
-    Given the name of the bucket and the filename(key), this function will return a dataframe.
+    Given the name of the bucket and the filename(key), this function will
+    return a dataframe.
     File to get MUST be json format.
-    :param bucket_name - String: Name of the s3 bucket
-    :param key - String: path & name of the file
-    :return data_df - DataFrame: Dataframe created from the json file in s3
+    :param bucket_name: Name of the s3 bucket - String
+    :param key: path & name of the file - String
+    :return data_df: Dataframe created from the json file in s3 - DataFrame
     """
-    s3 = boto3.resource('s3', region_name='eu-west-2')
+    s3 = boto3.resource("s3", region_name="eu-west-2")
     content_object = s3.Object(bucket_name, key)
-    file_content = content_object.get()['Body'].read().decode('utf-8')
+    file_content = content_object.get()["Body"].read().decode("utf-8")
     json_content = json.loads(file_content)
     data_df = pd.DataFrame(json_content)
 
@@ -45,55 +42,76 @@ def get_from_s3(bucket_name, key):
 def send_sqs_message(queue_url, message, sqs_messageid_name):
     """
     Will send an sqs message to a specified queue
-    :param queue_url - String: The url of the queue to send to.
-    :param message - String: The message to be sent
-                    (json string representation of dataframe)
-    :param sqs_messageid_name - String: The messageid to attach to the message
+    :param queue_url: The url of the queue to send to. - String
+    :param message: The message to be sent
+                            (json string representation of dataframe) - String
+    :param sqs_messageid_name: The messageid to attach to the message - String
     :return Nothing:
     """
-    sqs = boto3.client('sqs', region_name='eu-west-2')
-    sqs.send_message(QueueUrl=queue_url, MessageBody=message,
-                     MessageGroupId=sqs_messageid_name,
-                     MessageDeduplicationId=str(random.getrandbits(128)))
+    sqs = boto3.client("sqs", region_name="eu-west-2")
+    sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=message,
+        MessageGroupId=sqs_messageid_name,
+        MessageDeduplicationId=str(random.getrandbits(128)),
+    )
+
+
+class EnvironSchema(Schema):
+    bucket_name = fields.Str(required=True)
+    input_data = fields.Str(required=True)
+    queue_url = fields.Str(required=True)
+    arn = fields.Str(required=True)
+    sqs_messageid_name = fields.Str(required=True)
+    method_name = fields.Str(required=True)
+    identifier_column = fields.Str(required=True)
 
 
 def lambda_handler(event, context):
     """
-    Lambda function preparing data for enrichment and then calling the enrichment method.
-    :param event - String: Json string representing input
+    Lambda function preparing data for enrichment and then
+    calling the enrichment method.
+    :param event: Json string representing input - String
     :param context:
-    :return Json: success and checkpoint information, andor indication of error message.
+    :return Json: success and checkpoint information,
+                    andor indication of error message.
     """
     try:
-        checkpoint = event['RuntimeVariables']['checkpoint']
+        checkpoint = event["RuntimeVariables"]["checkpoint"]
+        schema = EnvironSchema()
+        config, errors = schema.load(os.environ)
+        if errors:
+            raise ValueError(f"Error validating environment params: {errors}")
+
         # S3
-        bucket_name = get_environment_variable('bucket_name')
-        input_data = get_environment_variable('input_data')
+        bucket_name = config["bucket_name"]
+        input_data = config["input_data"]
 
         # Set up client
-        lambda_client = boto3.client('lambda', region_name='eu-west-2')
+        lambda_client = boto3.client("lambda", region_name="eu-west-2")
 
         # Sqs
-        queue_url = get_environment_variable('queue_url')
+        queue_url = config["queue_url"]
 
         # Sns
-        arn = get_environment_variable('arn')
+        arn = config["arn"]
 
-        sqs_messageid_name = get_environment_variable('sqs_messageid_name')
-        method_name = get_environment_variable('method_name')
+        sqs_messageid_name = config["sqs_messageid_name"]
+        method_name = config["method_name"]
 
-        identifier_column = get_environment_variable('identifier_column')
+        identifier_column = config["identifier_column"]
 
         data_df = get_from_s3(bucket_name, input_data)
         wrangled_data = wrangle_data(data_df, identifier_column)
 
-        response = lambda_client.invoke(FunctionName=method_name,
-                                        Payload=json.dumps(wrangled_data))
-        json_response = response.get('Payload').read().decode("utf-8")
+        response = lambda_client.invoke(
+            FunctionName=method_name, Payload=json.dumps(wrangled_data)
+        )
+        json_response = response.get("Payload").read().decode("utf-8")
 
         final_output = json.loads(json_response)
-        anomalies = final_output['anomalies']
-        final_output = final_output['data']
+        anomalies = final_output["anomalies"]
+        final_output = final_output["data"]
 
         send_sqs_message(queue_url, final_output, sqs_messageid_name)
 
@@ -102,21 +120,13 @@ def lambda_handler(event, context):
 
     except Exception as exc:
 
-        sqs = boto3.client('sqs', region_name='eu-west-2')
-        sqs.purge_queue(
-            QueueUrl=queue_url
-        )
-
         return {
             "success": False,
             "checkpoint": checkpoint,
-            "error": "Unexpected Wrangler exception {}".format(_get_traceback(exc))
+            "error": "Unexpected Wrangler exception {}".format(_get_traceback(exc)),
         }
 
-    return {
-        "success": True,
-        "checkpoint": checkpoint
-    }
+    return {"success": True, "checkpoint": checkpoint}
 
 
 def send_sns_message(arn, anomalies, checkpoint):
@@ -126,39 +136,35 @@ def send_sns_message(arn, anomalies, checkpoint):
         module: Current module
         checkpoint: Module number of process
         anomalies: Data anomalies picked up during enrichment
-                    (non-failing but indicate problems with data)
+        (non-failing but indicate problems with data)
         message: Summary of metadata.
-    :param arn - String: Url of the sns queue
-    :param anomalies - String: json string representing data anomalies
-    :param checkpoint - int: Current 'module' of process
+    :param arn: Url of the sns queue - String
+    :param anomalies: json string representing data anomalies - String
+    :param checkpoint: Current 'module' of process - int
     :return Nothing:
     """
-    sns = boto3.client('sns', region_name='eu-west-2')
+    sns = boto3.client("sns", region_name="eu-west-2")
     sns_message = {
         "success": True,
         "module": "Enrichment",
         "checkpoint": checkpoint,
         "anomalies": anomalies,
-        "message": "Completed Enrichment"
-
+        "message": "Completed Enrichment",
     }
 
-    sns.publish(
-        TargetArn=arn,
-        Message=json.dumps(sns_message)
-    )
+    sns.publish(TargetArn=arn, Message=json.dumps(sns_message))
 
 
 def wrangle_data(data_df, identifier_column):
     """
     Prepares data for the enrichment step. May not be necessary going forward.
     Renames a column and returns df as json
-    :param data_df - DataFrame: Main input data to process
-    :param identifier_column - String: The name of the column
-                                representing unique id(usually responder_id)
-    :return data_json - String: Json String representation of the input dataframe.
+    :param data_df: Main input data to process - DataFrame
+    :param identifier_column: The name of the column representing unique id
+                    (usually responder_id) - String
+    :return data_json: Json String representation of the input dataframe. - String
     """
     data_df.rename(columns={"idbr": identifier_column}, inplace=True)
-    data_json = data_df.to_json(orient='records')
+    data_json = data_df.to_json(orient="records")
 
     return data_json
